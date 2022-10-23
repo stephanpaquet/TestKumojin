75a86aa37f5595772c3faf3c0070b1caf9a05598
Stephan Paquet <s.paquet@novatize.com>
2022-10-23 05:50:50 -0400

    Clean up App composnent

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,29 +1,5 @@
 <template>
     <div class="container w-100 m-auto  mt-3">
         <router-view></router-view>
-
-<!--        <list-view :events="events" class="text-center"/>-->
     </div>
 </template>
-
-<script>
-// import addItemForm from "./addItemForm";
-import createView from "./events/create";
-// import listView from "./events/index";
-
-// const routes = {
-//     '/': Home,
-//     '/about': About
-// }
-
-export default {
-    // components: {
-    //     // addItemForm,
-    //     listView
-    // },
-
-
-};
-</script>
-
-<style scoped></style>
